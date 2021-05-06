511c1b33819844b5d7f50497232424bce115be16
Danciak <fatality1922@gmail.com>
2021-05-06 20:21:02 +0200

    add amout of free seats counter

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -6,13 +6,12 @@ import './SeatChooser.scss';
 
 class SeatChooser extends React.Component {
   
-
-  
   componentDidMount() {
-    const { loadSeats } = this.props;
+    const { loadSeats, loadSeatsData } = this.props;
     this.socket = socket((process.env.NODE_ENV === 'production') ? 
     '/' : 'http://localhost:8000');
     loadSeats();
+    this.socket.on('seatsUpdated', (seats) => loadSeatsData(seats))
   }
 
   isTaken = (seatId) => {
@@ -33,7 +32,7 @@ class SeatChooser extends React.Component {
   render() {
 
     const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { requests, seats, chosenDay } = this.props;
 
     return (
       <div>
@@ -43,6 +42,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+        <h3>Free seats: {50 - seats.filter(item => item.day === chosenDay).length}/50</h3> 
       </div>
     )
   };
