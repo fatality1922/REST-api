da305fd8969868e26cf15c55ed39a730a9d8627f
Danciak <fatality1922@gmail.com>
2021-05-06 15:15:15 +0200

    update

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button, Progress, Alert } from 'reactstrap';
-
+import io from 'socket.io';
 import './SeatChooser.scss';
 
 class SeatChooser extends React.Component {
   
   componentDidMount() {
     const { loadSeats } = this.props;
+    this.socket = io((process.env.NODE_ENV === 'production') ? 
+    '/' : 'http://localhost:8000');
     loadSeats();
   }
 
